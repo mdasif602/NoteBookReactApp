f0aecc6fb3abcd8816b696cac5eb6d0055ed6491
Asif <mdasifhussain268@gmail.com>
2024-02-16 00:58:34 +0530

    created fetchUser endpoint

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,17 +4,18 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchUser');
 
 
 const JWT_SECRET = 'Asifisagoodb$oy';
 
 //ROUTE 1 :  create a User using: POST "/api/auth/createuser", Doesn't require auth No login required
 
-router.post('/createuser',[
-    body('name', 'Enter a valid name').isLength({min: 3}),
-    body('password', 'Enter a password of length minimum five').isLength({min: 5}),
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('password', 'Enter a password of length minimum five').isLength({ min: 5 }),
     body('email', 'Enter a valid Email').isEmail()
-], async (req, res)=>{
+], async (req, res) => {
     // console.log(req.body);
     // const user = User(req.body)
     // user.save();
@@ -27,10 +28,10 @@ router.post('/createuser',[
     }
 
     try {
-    // Check whether the user with this email exists already
-        let user = await User.findOne({email: req.body.email});
+        // Check whether the user with this email exists already
+        let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({error: "Sorry a user with this email already exists"});
+            return res.status(400).json({ error: "Sorry a user with this email already exists" });
         }
         const salt = await bcrypt.genSalt(10);
 
@@ -58,7 +59,7 @@ router.post('/createuser',[
         res.json(authToken);
 
     } catch (error) {
-        console.error(error.mesage); 
+        console.error(error.mesage);
         res.status(500).send("Intenal Server Error occured")
     }
 })
@@ -66,10 +67,10 @@ router.post('/createuser',[
 
 
 // ROUTE 2 : Authenticate a User using: POST "/api/auth/login", Doesn't require auth No login required
-router.post('/login',[
+router.post('/login', [
     body('email', 'Enter a valid Email').isEmail(),
     body('password', 'Password cannot be blank').exists()
-], async (req, res)=>{
+], async (req, res) => {
 
     // If there are errors, return Bad request and the errors
     const result = validationResult(req);
@@ -77,17 +78,17 @@ router.post('/login',[
         return res.status(400).json({ errors: result.array() });
     }
 
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
-        let user = await User.findOne({email});
+        let user = await User.findOne({ email });
         if (!user) {
-            return res.status(400).json({error: "Please try to login with correct credentials"});
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
         }
 
         const passwordCompare = await bcrypt.compare(password, user.password)
         if (!passwordCompare) {
-            return res.status(400).json({error: "Please try to login with correct credentials"});
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
         }
         const data = {
             user: {
@@ -98,9 +99,22 @@ router.post('/login',[
         const authToken = jwt.sign(data, JWT_SECRET);
         res.json(authToken);
     } catch (error) {
-        console.error(error.mesage); 
+        console.error(error.mesage);
         res.status(500).send("Intenal Server Error occured")
     }
-})
+});
+
+// ROUTE 3 : Get loggedIn user Detail using : POST "/api/auth/getUser". Login required
+router.post('/getUser', fetchuser, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error occured")
+    }
+
+});
 
 module.exports = router;
